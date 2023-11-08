90179f4398b140e6a4e8bc05e18b0f9591ef94bb
RendijsC <20098325@mail.wit.ie>
2023-11-08 12:23:03 +0000

    Bug Fix - make action icons on movie card confurable

--- movies/src/components/movieCard/index.js
+++ movies/src/components/movieCard/index.js
@@ -17,7 +17,7 @@ import img from '../../images/film-poster-placeholder.png'
 import React, { useContext  } from "react";
 import { MoviesContext } from "../../contexts/moviesContext";
 
-export default function MovieCard({ movie }) 
+export default function MovieCard({ movie, action }) 
   {
     const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -71,9 +71,7 @@ export default function MovieCard({ movie })
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
-        <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+        {action(movie)}
         <Link to={`/movies/${movie.id}`}>
           <Button variant="outlined" size="medium" color="primary">
             More Info ...
