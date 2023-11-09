2373ebcff1f2d941de6d116e22179147f55a996e
RendijsC <20098325@mail.wit.ie>
2023-11-09 15:17:24 +0000

    Added new review suppoert

--- movies/src/index.js
+++ movies/src/index.js
@@ -10,6 +10,8 @@ import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 import { QueryClientProvider, QueryClient } from "react-query";
 import { ReactQueryDevtools } from 'react-query/devtools';
 import MoviesContextProvider from "./contexts/moviesContext";
+import AddMovieReviewPage from './pages/addMovieReviewPage'
+
 
 
 const queryClient = new QueryClient({
@@ -36,6 +38,7 @@ const App = () => {
           <Route path="/movies/:id" element={<MoviePage />} />
           <Route path="/" element={<HomePage />} />
           <Route path="*" element={ <Navigate to="/" /> } />
+          <Route path="/reviews/form" element={ <AddMovieReviewPage /> } />
         </Routes>
         </MoviesContextProvider>
       </BrowserRouter>
