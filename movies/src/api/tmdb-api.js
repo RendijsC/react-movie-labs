94e2d50019222402a3e27bf31265a55be521d23a
RendijsC <20098325@mail.wit.ie>
2023-11-10 09:56:28 +0000

    Change upcoming movies page to use caching

--- movies/src/api/tmdb-api.js
+++ movies/src/api/tmdb-api.js
@@ -73,10 +73,11 @@ export const getMovie = (args) => {
       });
   };
 
+
   export const getUpcomingMovies = () => {
     return fetch(
       `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&page=1`
     )
-    .then(res => res.json())
-    .then(json => json.results);
+      .then((res) => res.json())
+      .then((json) => ({ results: json.results }));
   };
--- movies/src/pages/upcomingMoviesPage.js
+++ movies/src/pages/upcomingMoviesPage.js
@@ -1,36 +1,69 @@
-import React, { useState, useEffect } from "react";
-import PageTemplate from '../components/templateMovieListPage';
-import { getUpcomingMovies } from "../api/tmdb-api"
-//import AddToFavoritesIcon from '../components/cardIcons/addToFavorites';
-import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+// import React, { useState, useEffect } from "react";
+// import PageTemplate from '../components/templateMovieListPage';
+// import { getUpcomingMovies } from "../api/tmdb-api"
+// import PlaylistAddIcon from '@mui/icons-material/PlaylistAdd';
+
+// const UpcomingMoviesPage = (props) => {
+//   const [movies, setMovies] = useState([]);
+//   const favorites = movies.filter(m => m.favorite)
+//   localStorage.setItem('favorites', JSON.stringify(favorites))
+
+  
+//   useEffect(() => {
+//     getUpcomingMovies().then(movies => {
+//       setMovies(movies);
+//     });
+//   }, []);
+
+//   return (
+//     <PageTemplate
+//       title='Upcoming Movies'
+//       movies={movies}
+//       action={(movie) => {
+//         return <PlaylistAddIcon movie={movie} />;
+//       }}
+      
+//     />
+//   );
+// };
+// export default UpcomingMoviesPage;
+
+import React from "react";
+import PageTemplate from "../components/templateMovieListPage";
+import { useQuery } from "react-query";
+import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import Spinner from "../components/spinner";
+import { getUpcomingMovies } from "../api/tmdb-api";
 
 const UpcomingMoviesPage = (props) => {
-  const [movies, setMovies] = useState([]);
-  const favorites = movies.filter(m => m.favorite)
-  localStorage.setItem('favorites', JSON.stringify(favorites))
+  const { data, error, isLoading, isError } = useQuery(
+    "upcomingMovies",
+    getUpcomingMovies
+  );
 
-  const addToFavorites = (movieId) => {
-    const updatedMovies = movies.map((m) =>
-      m.id === movieId ? { ...m, favorite: true } : m
-    );
-    setMovies(updatedMovies);
-  };
+  if (isLoading) {
+    return <Spinner />;
+  }
 
-  useEffect(() => {
-    getUpcomingMovies().then(movies => {
-      setMovies(movies);
-    });
-  }, []);
+  if (isError) {
+    return <h1>{error.message}</h1>;
+  }
+
+  const movies = data.results;
+
+  // Redundant, but necessary to avoid app crashing.
+  const favorites = movies.filter((m) => m.favorite);
+  localStorage.setItem("favorites", JSON.stringify(favorites));
 
   return (
     <PageTemplate
-      title='Upcoming Movies'
+      title="Upcoming Movies"
       movies={movies}
       action={(movie) => {
         return <PlaylistAddIcon movie={movie} />;
       }}
-      
     />
   );
 };
+
 export default UpcomingMoviesPage;
